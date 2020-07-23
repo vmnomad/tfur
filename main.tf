b08d20360c2b1c475905a7e106d661484d2a2ab5
Askar Kopbayev <vmnomad@AK-MacBook-Pro.local>
2020-07-23 22:11:55 +1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "vm" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+
+}
+
