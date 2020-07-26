b4aeefec6f67863056fd238fd252f3474468cf64
Askar Kopbayev <vmnomad@AK-MacBook-Pro.local>
2020-07-26 11:25:30 +1000

    Add web server config and security group to limit access to TCP/8080

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -19,13 +20,37 @@ data "aws_ami" "ubuntu" {
 }
 
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 resource "aws_instance" "vm" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
 
   tags = {
     Name = "terraform-example"
   }
+
+
+
 
 }
 
